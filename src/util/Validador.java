4ca0f714dba7b4cc88fd7f27278342599bb2fe5f
SaulGarciaaaa <152623929+SaulGarciaaaa@users.noreply.github.com>
2026-01-19 22:21:27 +0000

    Añadida clase validador

---
+++ src/util/Validador.java
@@ -0,0 +1,11 @@
+package util;
+
+
+import model.Usuario;
+
+public class Validador {
+    public static boolean validarUsuario(Usuario usuario) {
+            return usuario != null && usuario.getNombre() != null;
+        }
+    }
+
