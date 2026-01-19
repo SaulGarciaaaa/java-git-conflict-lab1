125c90b53777bdc96b6c7efd8a43874ba0b3dcc2
SaulGarciaaaa <152623929+SaulGarciaaaa@users.noreply.github.com>
2026-01-19 22:38:55 +0000

    Cambios enatributos

--- src/model/Usuario.java
+++ src/model/Usuario.java
@@ -1,10 +1,17 @@
 package model;
 
 public class Usuario {
+    private int id;
     private String email;
     private String nombre;
 
-    public Usuario(String email, String nombre) {
+    public Usuario(int id, String nombre) {
+        this.id = id;
+        this.nombre = nombre;
+    }
+
+    public Usuario(int id, String email, String nombre) {
+        this.id = id;
         this.email = email;
         this.nombre = nombre;
     }
@@ -15,5 +22,21 @@ public class Usuario {
 
     public String getNombre() {
         return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 }
